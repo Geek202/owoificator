4af27d0a52203bac1eab9a5381a89c062fd0a1db
Tom_The_Geek <tgbryan39@gmail.com>
2020-12-14 20:58:35 +0000

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="App">
+    <header class="App-header">
+      <h1>Owoify</h1>
+      <Owoifier />
+    </header>
+  </div>
+</template>
+
+<script lang="ts">
+import {defineComponent, reactive} from 'vue';
+import FooTsxVue from './components/FooTsx.vue';
+import FooTsx from './components/Foo';
+import BarJsxVue from './components/BarJsx.vue';
+import BarJsx from './components/Bar';
+import Owoifier from './components/Owoifier.vue';
+
+interface State {
+  message: string;
+}
+
+export default defineComponent({
+  components: {
+    FooTsxVue,
+    FooTsx,
+    BarJsxVue,
+    BarJsx,
+    Owoifier,
+  },
+  setup() {
+    const state = reactive({
+      message: 'Learn Vue',
+    });
+    return {
+      state,
+    };
+  },
+});
+</script>
+
+<style>
+.App {
+  text-align: center;
+}
+.App-header {
+  background-color: #f9f6f6;
+  color: #32485f;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+}
+.App-link {
+  color: #00c185;
+}
+.App-logo {
+  height: 40vmin;
+  pointer-events: none;
+  margin-bottom: 1rem;
+  animation: App-logo-spin infinite 1.6s ease-in-out alternate;
+}
+.App-tsx {
+  display: flex;
+}
+.App-tsx > div {
+  margin-left: 30px;
+  font-size: 16px;
+}
+@keyframes App-logo-spin {
+  from {
+    transform: scale(1);
+  }
+  to {
+    transform: scale(1.06);
+  }
+}
+</style>
---
+++ src/components/Owoifier.vue
@@ -0,0 +1,38 @@
+<template>
+    <input v-model="message" type="text" placeholder="Enter text to owo">
+    <h3>Owoed text:</h3>
+    <p>Low: {{ owoedText }}<br>
+    Moderate: {{ uwuedText }}<br>
+    High: {{ veryUwuedText }}<br>
+    Extweme: {{ extremeUwuedText }}</p>
+</template>
+
+<script>
+import {defineComponent, reactive} from 'vue';
+import { translate as owoify } from '@zuzak/owo';
+import { default as owo2 } from 'owoify-js';
+
+export default defineComponent({
+  data() {
+    return {
+      message: '',
+    }
+  },
+
+  computed: {
+      owoedText() {
+          return owoify(this.message)
+      },
+      uwuedText() {
+          return owo2(this.message)
+      },
+      veryUwuedText() {
+          return owo2(this.message, 'uwu')
+      },
+      extremeUwuedText() {
+          return owo2(this.message, 'uvu')
+      }
+  }
+});
+
+</script>
